8631188b6492a87eb615f9cc02f757f6f24c8cb0
GeneTv <contact@genetv.net>
2022-01-13 08:21:41 +0100

    Kinda finished idb wrapper

--- src/common/indexed-db.js
+++ src/common/indexed-db.js
@@ -1,6 +1,5 @@
 const DB_NAME = 'ow-stats';
 const DB_VERSION = 1;
-
 let database;
 
 const dbCreate = () => {
@@ -10,7 +9,6 @@ const dbCreate = () => {
 
     dbRequest.onupgradeneeded = event => {
       database = event.target.result;
-
       // Here we are going to create all stores / collections.
       database.createObjectStore('games', { keyPath: 'id' }); // keyPath defines a property of the object as the id property.
     }
@@ -29,17 +27,24 @@ const dbWrite = (storeName, object) => {
 }
 
 const dbRead = (storeName) => {
-  const transaction = database.transaction(storeName, 'readonly');
-  const store = transaction.objectStore(storeName);
-  const readRequest = store.openCursor();
+  return new Promise((resolve, reject) => {
+    const transaction = database.transaction(storeName, 'readonly');
+    const store = transaction.objectStore(storeName);
+    const readRequest = store.openCursor();
+    let items = [];
 
-  readRequest.onsuccess = event => {
-    const cursor = event.target.result;
-    if(cursor) {
-      console.log('Cursor', cursor);
-      cursor.continue();
+    readRequest.onerror = reject;
+
+    readRequest.onsuccess = event => {
+      const cursor = event.target.result;
+      if(cursor) {
+        items.push(cursor.value);
+        cursor.continue();
+        return;
+      }
+      resolve(items);
     }
-  }
+  })
 }
 
 export { dbCreate, dbWrite, dbRead };
