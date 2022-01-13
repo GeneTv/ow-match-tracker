b7092634b87b0b1d0bd7f06288454971c4101a7c
GeneTv <contact@genetv.net>
2022-01-13 08:44:02 +0100

    Stuff kinda starts working now

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,23 +3,46 @@
 </template>
 
 <script>
-import { dbCreate, dbWrite, dbRead } from '@/common/indexed-db';
+/* eslint-disable */
+import { dbCreate, dbWrite, dbRead } from '@/services/idb.service';
 
 export default {
   name: 'Dashboard',
-  mounted() {
-    dbCreate().then( () => {
-      dbWrite('games', {
-        id: 2,
-        start: 'idk',
-        end: 'idk',
-        kills: 15,
-        deaths: 9023420 // fucking feeder
-      });
+  async mounted() {
+    await dbCreate();
 
-      dbRead('games');
+    await dbWrite('accounts', {
+      battletag: "Oicred007#2118",
+      username: "Oicred007",
+      disciminator: 2118,
+      gamesCount: 18,
+      skillRating: {
+        openQueue: 1432,
+        tank: 1932,
+        dps: 1245,
+        support: 4923
+      },
+      mostPlayedChars: ["Roadhog", "Winston", "Sombra"],
+      lastLogged: "2022-01-13"
+    });
 
-    })
+    await dbWrite('accounts', {
+      battletag: "ƓǾMMȆHD#2521",
+      username: "ƓǾMMȆHD",
+      disciminator: 2521,
+      gamesCount: 22,
+      skillRatings: {
+        openQueue: 1842,
+        tank: 2774,
+        dps: 2376,
+        support: 2723
+      },
+      mostPlayedChars: ["Ana", "Winston", "Mercy"],
+      lastLogged: "2022-01-12"
+    });
+
+    let games = await dbRead('accounts');
+    console.log('Accounts:', games);
   }
 }
 </script>
